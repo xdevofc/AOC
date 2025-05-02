991f912f2f0e7f975ffb6db7f7b055df88202e71
xdev <xdevofc@gmail.com>
2025-05-02 14:08:18 -0300

    Solving day3 secondChallenge

--- 2015/golang/day3/main.go
+++ 2015/golang/day3/main.go
@@ -5,12 +5,12 @@ import (
     "os"
 )
 // helpers 
-func positionExist(c map[string]bool, clave string, posiciones *Coordenadas){
+func positionExist(c map[string]bool, clave string, position *Coordenadas){
     
     if c[clave] == true{
         fmt.Println("Ya existe")
     }else{
-        ContarCasas(posiciones)
+        ContarCasas(position)
         c[clave] = true
     }
 }
@@ -21,14 +21,22 @@ func main (){
    
     // declaracion de la estructura
 
-    posiciones := Coordenadas{x: 1, y:1, houses:1}
+    posicionesSanta := Coordenadas{x: 1, y:1, houses:0}
+    posicionesRobot := Coordenadas{x:1, y:1, houses:0}
     
     // creamos un map para guardar las coordenadas ingresadas 
+    
     coordenadas_registradas := make(map[string]bool)
     // sprintf formatea la cadena a texto
-    clave := fmt.Sprintf("%d,%d", posiciones.x, posiciones.y)
-
+    clave := fmt.Sprintf("%d,%d", posicionesSanta.x, posicionesSanta.y)
+    
+    // creamos el camino que recorre santa y el roboto 
     coordenadas_registradas[clave] = true
+    ContarCasas(&posicionesRobot)
+    ContarCasas(&posicionesSanta)
+    //turno 
+
+    turno := 0
 
 
     // manipulamos el archivo
@@ -49,31 +57,81 @@ func main (){
          
 
         if string(c) == "^" {
-            MoverArriba(&posiciones)
-            clave := fmt.Sprintf("%d,%d", posiciones.x, posiciones.y)
-            positionExist(coordenadas_registradas, clave, &posiciones)
+        // verificar quien da el siguiente paso
+            if turno == 0 {
+                MoverArriba(&posicionesSanta)
+                clave := fmt.Sprintf("%d,%d", posicionesSanta.x, posicionesSanta.y)
+                positionExist(coordenadas_registradas, clave, &posicionesSanta)
+                turno +=1
+            }else{
+                MoverArriba(&posicionesRobot)
+                claveRobot := fmt.Sprintf("%d,%d", posicionesRobot.x, posicionesRobot.y)
+                positionExist(coordenadas_registradas, claveRobot, &posicionesRobot)
+                turno -= 1 
+            }
+           
+
         }else if string(c) == "v"{
-            MoverAbajo(&posiciones)
-            clave := fmt.Sprintf("%d,%d", posiciones.x, posiciones.y) 
-            positionExist(coordenadas_registradas, clave, &posiciones)
+                 // verificar quien da el siguiente paso
+                if turno == 0 {
+                    MoverAbajo(&posicionesSanta)
+                    clave := fmt.Sprintf("%d,%d", posicionesSanta.x, posicionesSanta.y) 
+                    positionExist(coordenadas_registradas, clave, &posicionesSanta)
+                    turno +=1 
+               }else{
+                    MoverAbajo(&posicionesRobot)
+                    claveRobot := fmt.Sprintf("%d,%d", posicionesRobot.x, posicionesRobot.y)
+                    positionExist(coordenadas_registradas, claveRobot, &posicionesRobot)
+                    turno -= 1 
+                }
+
+
         }else if string(c) == ">"{
-            MoverDerecha(&posiciones)
-            clave := fmt.Sprintf("%d,%d", posiciones.x, posiciones.y)
-            positionExist(coordenadas_registradas, clave, &posiciones)
+                 // verificar quien da el siguiente paso
+                if turno == 0 {
+                    MoverDerecha(&posicionesSanta)
+                    clave := fmt.Sprintf("%d,%d", posicionesSanta.x, posicionesSanta.y) 
+                    positionExist(coordenadas_registradas, clave, &posicionesSanta)
+                    turno +=1 
+               }else{
+                   MoverDerecha(&posicionesRobot)
+                    claveRobot := fmt.Sprintf("%d,%d", posicionesRobot.x, posicionesRobot.y)
+                    positionExist(coordenadas_registradas, claveRobot, &posicionesRobot)
+                    turno -= 1 
+                }
+
         }else if string(c) == "<"{
-            MoverIzquierda(&posiciones)
-            clave := fmt.Sprintf("%d,%d", posiciones.x, posiciones.y)
-            positionExist(coordenadas_registradas, clave, &posiciones)
+                 // verificar quien da el siguiente paso
+                if turno == 0 {
+                    MoverIzquierda(&posicionesSanta)
+                    clave := fmt.Sprintf("%d,%d", posicionesSanta.x, posicionesSanta.y) 
+                    positionExist(coordenadas_registradas, clave, &posicionesSanta)
+                    turno +=1 
+               }else{
+                   MoverIzquierda(&posicionesRobot)
+                    claveRobot := fmt.Sprintf("%d,%d", posicionesRobot.x, posicionesRobot.y)
+                    positionExist(coordenadas_registradas, claveRobot, &posicionesRobot)
+                    turno -= 1 
+                }
+
         }
+        
 
              
-        fmt.Printf("La posicione en x: %d Y:%d Casas:%d \n",
-            posiciones.x,
-            posiciones.y,
-            posiciones.houses)
+        fmt.Printf("La posiciones SANTA: en x: %d Y:%d Casas:%d \n",
+            posicionesSanta.x,
+            posicionesSanta.y,
+            posicionesSanta.houses)
 
+     
+            fmt.Printf("La posiciones ROBOT: en x: %d Y:%d Casas:%d \n",
+            posicionesRobot.x,
+            posicionesRobot.y,
+            posicionesRobot.houses)
 
-// testing
+            
+            fmt.Println("La suma de casas es:", len(coordenadas_registradas))
+
 
     }
 
