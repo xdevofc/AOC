38fba8f03c8721f2ae7c8f15ebcc38894eda1399
xdev <xdevofc@gmail.com>
2025-05-07 19:15:31 -0300

    Solving day 4 first star, hashing

--- 2015/golang/day4/main.go
+++ 2015/golang/day4/main.go
@@ -1,11 +1,49 @@
-package main 
+package main
 
 import (
-    "fmt"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
 func main(){
-    fmt.Println("Hello from day4")
-    SecondChallenge()
+
+	input := "bgvyzdsv"
+	var numero = 1
+	
+	// creando un loop para encontrar el numero que hace que empieze con 00000 
+
+	for {
+
+		// pasar del numero a string
+		num_string := strconv.Itoa(numero)
+		
+		// unir cadenas
+		clave := input + num_string
+
+		// obtener hash directamente
+		// encryptando a md5 la cadena de texto
+		hash := md5.Sum([]byte(clave))
+
+		// convertir el arreglo a string
+
+		decrypt := hex.EncodeToString(hash[:])
+
+
+		if strings.HasPrefix(decrypt, "00000"){
+			
+			fmt.Printf("La clave es: %v\n ", decrypt)
+			fmt.Printf("El numero es: %v\n ", numero)
+			break
+		}
+
+		numero +=1
+
+
+} 
+    
+	// SecondChallenge()
 }
 
